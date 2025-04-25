95553c8b7a150c1ca2c61ac6a7b639cca569ed71
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-25 13:56:05 +0700

    pembuatan text editing controller

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1_flutter/pages/home_page.dart';
+import 'package:ucp1_flutter/pages/registrasi_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold();
+  }
+}
---
+++ lib/pages/registrasi_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistrasiPage extends StatefulWidget {
+  const RegistrasiPage({super.key});
+
+  @override
+  State<RegistrasiPage> createState() => _RegistrasiPageState();
+}
+
+class _RegistrasiPageState extends State<RegistrasiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
