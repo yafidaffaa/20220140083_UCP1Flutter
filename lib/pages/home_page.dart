b07c7b88fc595b0f2a427ead344b54b62d7a19ba
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-25 15:50:52 +0700

    menambahkan image pada home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ucp1_flutter/pages/login_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,16 +13,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          leading: SizedBox(
+            width: 100,
+            height: 100,
+            child: Image.asset('assets/logo_umy.png', width: 100, height: 100),
+          ),
+          title: const Text('Home Page'),
+          backgroundColor: CupertinoColors.activeGreen,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Text(
-              'Welcome to the Home Page!',
-              style: TextStyle(fontSize: 24),
-            ),
+            Image.asset('assets/promosi_umy.jpg', width: 400, height: 200),
             const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(width: 10),
+                Image.asset('assets/logo_umy.png', width: 50, height: 50),
+              ],
+            ),
           ],
         ),
       ),
--- lib/pages/registrasi_page.dart
+++ lib/pages/registrasi_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:ucp1_flutter/pages/login_page.dart';
 
