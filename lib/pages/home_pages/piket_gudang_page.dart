0f1711d2d7c7d4e7c5d0323cdc08eb6cd84380c4
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-27 12:10:06 +0700

    membuat piket gudang page

---
+++ lib/pages/home_pages/piket_gudang_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PiketGudangPage extends StatefulWidget {
+  const PiketGudangPage({super.key});
+
+  @override
+  State<PiketGudangPage> createState() => _PiketGudangPageState();
+}
+
+class _PiketGudangPageState extends State<PiketGudangPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
