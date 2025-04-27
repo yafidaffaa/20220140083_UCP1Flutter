b8d056a54725e0a6922e7ea0978a97720020e4eb
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-04-27 10:49:45 +0700

    ganti jenis stateless untuk homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,45 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ucp1_flutter/pages/login_page.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  final String email;
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  const HomePage({super.key, required this.email});
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 200,
-              decoration: const BoxDecoration(color: Colors.blue),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.arrow_back),
-                  ),
-                ],
-              ),
-            ),
-            Image.asset('assets/promosi_umy.jpg', width: 400, height: 200),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(width: 10),
-                Image.asset('assets/logo_umy.png', width: 50, height: 50),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
